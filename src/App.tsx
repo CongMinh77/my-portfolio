1270777831ef1ea0b39978b22d1a38d8a8ab06cc
Nguyen Cong Minh <minhnc@deha-soft.com>
2023-11-15 22:31:12 +0700

    add i18n

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import './App.css'
-import Routing from './routes/routing'
+import "./App.css"
+import LanguageSelector from "./components/LanguageSelector"
+import Routing from "./routes/routing"
+import { useTranslation } from "react-i18next"
 
 function App() {
+  const { t } = useTranslation()
   return (
     <div className="App">
       <header className="App-header">
+        <LanguageSelector />
+        <h2>{t("app.title")}</h2>
         <Routing />
       </header>
     </div>
---
+++ src/components/LanguageSelector/LanguageSelector.tsx
@@ -0,0 +1,19 @@
+import { useTranslation } from "react-i18next"
+
+const LanguageSelector = () => {
+  const [t, i18n] = useTranslation()
+  return (
+    <div style={{ display: "flex" }}>
+      <span>{t("language-selector.label")} </span>
+      <button onClick={() => i18n.changeLanguage("vi")}>
+        {t("language-selector.languages.vi")}
+      </button>
+      &nbsp;
+      <button onClick={() => i18n.changeLanguage("en")}>
+        {t("language-selector.languages.en")}
+      </button>
+    </div>
+  )
+}
+
+export default LanguageSelector
