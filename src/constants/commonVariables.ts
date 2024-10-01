b48477070086cbd960497484ec3d2f685d6d3e79
Nguyen Cong Minh <minhnc@deha-soft.com>
2024-10-01 23:56:57 +0700

    update rule big small game

---
+++ src/constants/commonVariables.ts
@@ -0,0 +1,4 @@
+export const TYPE_RESULT_ROLL = {
+  SMALL: 1,
+  BIG: 2
+}
--- src/widgets/BigSmallGame/BigSmallGame.tsx
+++ src/widgets/BigSmallGame/BigSmallGame.tsx
@@ -1,5 +1,6 @@
 import { Box, Button, Typography } from '@mui/material'
 import React, { useEffect, useState } from 'react'
+import { TYPE_RESULT_ROLL } from '../../constants/commonVariables'
 
 type IProps = {}
 
@@ -14,17 +15,39 @@ export default function BigSmallGame({}: IProps) {
     { dice: 0 }
   ])
 
-  const handleRandomDice = () => {
-    setResults([
-      { dice: (Math.floor(Math.random() * 6) + 1) as number },
-      { dice: (Math.floor(Math.random() * 6) + 1) as number },
-      { dice: (Math.floor(Math.random() * 6) + 1) as number }
-    ])
+  const handleRandomDice = (typeResult?: number) => {
+    const number = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18]
+    if (typeResult === TYPE_RESULT_ROLL.SMALL) {
+      const lastNumber = Math.floor(Math.random() * 3) + 1
+      setResults([
+        { dice: (Math.floor(Math.random() * 3) + 1) as number },
+        { dice: (Math.floor(Math.random() * 3) + 1) as number },
+        { dice: (Math.floor(Math.random() * 3) + 1) as number }
+      ])
+    } else if (typeResult === TYPE_RESULT_ROLL.BIG) {
+      setResults([
+        { dice: (Math.floor(Math.random() * 6) + 1) as number },
+        { dice: (Math.floor(Math.random() * 6) + 1) as number },
+        { dice: (Math.floor(Math.random() * 6) + 1) as number }
+      ])
+    } else {
+      setResults([
+        { dice: (Math.floor(Math.random() * 6) + 1) as number },
+        { dice: (Math.floor(Math.random() * 6) + 1) as number },
+        { dice: (Math.floor(Math.random() * 6) + 1) as number }
+      ])
+    }
   }
 
   return (
     <Box p={2}>
-      <Button onClick={handleRandomDice}>Random dice</Button>
+      <Button onClick={() => handleRandomDice()}>Random dice</Button>
+      <Button onClick={() => handleRandomDice(TYPE_RESULT_ROLL.SMALL)}>
+        Small result
+      </Button>
+      <Button onClick={() => handleRandomDice(TYPE_RESULT_ROLL.BIG)}>
+        Big result
+      </Button>
       {results.map((item: IPropResults) => (
         <Typography>{item.dice}</Typography>
       ))}
