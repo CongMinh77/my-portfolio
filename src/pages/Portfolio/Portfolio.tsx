586e40cc596ac1f8ef4e63f997ba2d32fb952085
Nguyen Cong Minh <cong.minh7.7.2000@gmail.com>
2025-11-09 13:28:08 +0700

    update format

--- src/pages/Portfolio/Portfolio.tsx
+++ src/pages/Portfolio/Portfolio.tsx
@@ -1,29 +1,29 @@
-import React from 'react'
-import { useTranslation } from 'react-i18next'
-import PortfolioGallery from '../../components/PortfolioGallery'
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import PortfolioGallery from '../../components/PortfolioGallery';
 
 // Sử dụng hình ảnh cục bộ thay vì từ Unsplash
 const images = [
   {
     src: '/assets/portfolio/prj-steny.png',
     title: 'Prj Steny',
-    website: 'https://stenyorganic.com/'
+    website: 'https://stenyorganic.com/',
   },
   {
     src: '/assets/portfolio/prj-god-of-decisions.png',
     title: 'Prj God Of Decisions',
-    website: 'https://god-of-decisions.vercel.app/'
-  }
+    website: 'https://god-of-decisions.vercel.app/',
+  },
   // {
   //   src: '/assets/portfolio/prj-flip-book.png',
   //   title: 'Prj Flip Book',
   //   website: 'https://flip-book-nu.vercel.app/'
   // }
-]
+];
 const Portfolio = () => {
-  const { t } = useTranslation()
+  const { t } = useTranslation();
 
-  return <PortfolioGallery images={images} title={t('my_portfolio.title')} />
-}
+  return <PortfolioGallery images={images} title={t('my_portfolio.title')} />;
+};
 
-export default Portfolio
+export default Portfolio;
